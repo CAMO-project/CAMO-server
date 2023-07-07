9e4062677f28aa622ef9661b1b53477e632bf785
jcw1031 <jcw001031@gmail.com>
2023-07-07 16:49:34 +0900

    initial commit

---
+++ src/main/java/team/moca/camo/CamoApplication.java
@@ -0,0 +1,13 @@
+package team.moca.camo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/moca/camo/CamoApplicationTests.java
@@ -0,0 +1,13 @@
+package team.moca.camo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

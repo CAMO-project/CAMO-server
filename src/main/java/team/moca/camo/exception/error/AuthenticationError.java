ce2c6877ec68eb78a09a774bdc5218b66c7ee9d9
jcw1031 <jcw001031@gmail.com>
2023-08-04 22:59:15 +0900

    AuthenticationError 에러 코드 수정

--- src/main/java/team/moca/camo/exception/error/AuthenticationError.java
+++ src/main/java/team/moca/camo/exception/error/AuthenticationError.java
@@ -8,10 +8,10 @@ public enum AuthenticationError implements CamoError {
 
     USER_AUTHENTICATION_FAIL("C0001", "회원 인증에 실패하였습니다."),
     INVALID_TOKEN_ERROR("C0002", "유효하지 않은 토큰입니다."),
-    EMAIL_DUPLICATION("C003", "중복된 이메일입니다."),
-    PHONE_DUPLICATION("C004", "중복된 전화번호입니다."),
-    NICKNAME_DUPLICATION("C005", "중복된 닉네임입니다."),
-    PASSWORD_CHECK_MISMATCH("C006", "비밀번호 확인이 일치하지 않습니다.");
+    EMAIL_DUPLICATION("C0003", "중복된 이메일입니다."),
+    PHONE_DUPLICATION("C0004", "중복된 전화번호입니다."),
+    NICKNAME_DUPLICATION("C0005", "중복된 닉네임입니다."),
+    PASSWORD_CHECK_MISMATCH("C0006", "비밀번호 확인이 일치하지 않습니다.");
 
     private final String errorCode;
     private final String message;
